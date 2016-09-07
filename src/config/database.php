341e8c9c708844e756384ed21b83b7c979686a6a
hainuo <admin@hainuo.info>
2016-09-07 16:09:30 +0800

    简单修改 bak

--- src/config/database.php
+++ src/config/database.php
@@ -5,10 +5,20 @@
  * Date: 16-7-15
  * Time: 下午4:49
  */
-$config['database']['active'] = 'test';
-$config['database']['test']['host'] = 'localhost';
-$config['database']['test']['user'] = 'test';
-$config['database']['test']['password'] = 'test';
-$config['database']['test']['database'] = 'test';
-$config['database']['asyn_max_count'] = 10;
+$config[ 'database' ] = [
+	'active'         => 'test',
+	'test'           => [
+		'host'     => 'localhost',
+		'user'     => 'root',
+		'password' => '',
+		'database' => 'wease',
+	],
+	'product'        => [
+		'host'     => 'localhost',
+		'user'     => 'root',
+		'password' => '',
+		'database' => 'wease',
+	],
+	'asyn_max_count' => 10,
+];
 return $config;
